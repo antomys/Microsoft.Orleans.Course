ecaad1dbd9cc1b948f5c1d5a302deb4b37e195f0
ihor.volokhovych <ihor.volokhovych@pm.bet>
2023-02-26 19:04:00 +0200

    feat(#4): added DbUp migrator

---
+++ src/Microsoft.Orleans.Course.Db.Migration/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Orleans.Course.Db.Migration;
+
+var connectionString =
+    args.FirstOrDefault()
+    ?? "Server=localhost;Database=Microsoft_Orleans_Course;Uid=root;Pwd=password;";
+
+var engine = connectionString.BuildMySqlEngine();
+
+var result = engine.PerformUpgrade();
+
+if (!result.Successful)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(result.Error);
+    Console.ResetColor();
+#if DEBUG
+    Console.ReadLine();
+#endif                
+    return -1;
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Success!");
+Console.ResetColor();
+
+return 0;
